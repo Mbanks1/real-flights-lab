a1c6a18dd8af16182285d2bdacdbe3da4dda8e51
Mbanks1 <michael1banks@icloud.com>
2020-12-05 19:08:15 -0400

    complete schema

--- models/flight.js
+++ models/flight.js
@@ -2,8 +2,14 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema
 
 const flightSchema = new Schema({
+
     airline: {type: String, enum: ['American', 'Southwest', 'United']},
     airport: {type: String, enum: ['AUS', 'DFW','DEN', 'LAX', 'SAN'], 'DEN'},
     flightNo: {type: Number, required: true, min: 10, max: 9999},
-    departs: {type: Date, default: ()=> { return }}
-})
+    departs: {type: Date, default: ()=> Date.now() + 365 * 24 * 60 * 60 * 1000
+    },
+},{
+    timestamps: true,
+}
+);
+
