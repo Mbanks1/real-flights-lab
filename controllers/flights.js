5a705e56788bd8f52213ab5778ed086682c82019
Mbanks1 <michael1banks@icloud.com>
2020-12-06 19:10:13 -0400

    controller function

--- controllers/flights.js
+++ controllers/flights.js
@@ -1,2 +1,17 @@
 const Flight = require('../models/flight');
 
+module.exports = {
+ index,
+
+}
+
+
+function index(req, res){
+    Flight.find({}, function(err, flights){
+        res.render('/flights/index')
+    })
+};
+
+
+
+
