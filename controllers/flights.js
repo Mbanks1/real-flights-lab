2e5c194f5e8a12a8fc038feed37c1b2de3e804f8
Mbanks1 <michael1banks@icloud.com>
2020-12-06 20:40:17 -0400

    new flight form

--- controllers/flights.js
+++ controllers/flights.js
@@ -17,6 +17,22 @@ function newFlight(req, res){
     res.render('flights/new', {title: 'Add Flight', err: 'Error!'})
 };
 
+function create(req, res) {
+    req.body.newFlight = !!req.body.newFlight
+    for (let key in req.body) {
+        if (req.body[key] === '') delete req.body[key]
+      }
+      const flight = new Flight(req.body)
+      flight.save(function(err) {
+        if (err) {
+          console.log(err)
+          return res.render('flights/new', {err: err})
+        }
+        console.log(flight)
+        res.redirect(`/flights/${flights._id}`)
+      })
+    };
+    
 
 
 
